66a58d3408b8e552087678cc753166c759ae1451
Gustavo H <gvhenrique@hotmail.com>
2022-07-06 11:32:47 -0300

    feat: input select component finished

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import Input from "../../components/Input/index.jsx";
+import SelectInput from "../../components/SelectInput/index.jsx";
 
 function Home() {
 	return (
 		<div>
 			{/* Exemplos Input */}
-			{/* <Input
+			<Input
 				label="Senha"
 				name="password"
 				type="password"
@@ -19,7 +20,12 @@ function Home() {
 				placeholder="Digite aqui seu nome"
 				// register={register}
 				// error={errors.password?.message}
-			/> */}
+			/>
+			<SelectInput
+				label="Atribuição"
+				name="atribution"
+				// register={register}
+			/>
 		</div>
 	);
 }
