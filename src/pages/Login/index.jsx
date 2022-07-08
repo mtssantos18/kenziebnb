9664f9bcea42292aa9c5802d21306a87e364fe27
Matheus Santos <eumtssantos@outlook.com>
2022-07-08 15:08:45 -0300

    feat: implemented animation for the loginForm page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,22 +1,22 @@
 import LoginForm from "../../components/LoginForm";
 import {
-	Background,
-	ImageContainer,
-	LoginModal,
-	ScreenContainer,
+  Background,
+  ImageContainer,
+  LoginModal,
+  ScreenContainer,
 } from "./style";
 import ubatubaImg from "../../assets/Ubatuba1.png";
 
 function Login() {
-	return (
-		<ScreenContainer>
-			<LoginModal>
-				<img src={ubatubaImg} />
+  return (
+    <ScreenContainer>
+      <LoginModal>
+        <img src={ubatubaImg} />
 
-				<LoginForm />
-			</LoginModal>
-		</ScreenContainer>
-	);
+        <LoginForm />
+      </LoginModal>
+    </ScreenContainer>
+  );
 }
 
 export default Login;
