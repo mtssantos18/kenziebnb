293c8b05495c2e96d4e5a48ff5777ed8fa06645a
Matheus Santos <eumtssantos@outlook.com>
2022-07-12 07:35:52 -0300

    fix: just indenting home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,11 @@ import Header from "./../../components/Header";
 import CardHouse from "../../components/CardHouse/index.js";
 
 import {
-	List,
-	Container,
-	FilterButton,
-	ContentContainer,
-	FooterContainer,
+  List,
+  Container,
+  FilterButton,
+  ContentContainer,
+  FooterContainer,
 } from "./styles.js";
 
 import mockup from "./../../images/mockup.png";
@@ -22,47 +22,47 @@ import { HiOutlineEmojiSad } from "react-icons/hi";
 import { FilterContext } from "../../providers/Filter/Filter";
 
 function Home() {
-	const { homeList, showFilterModal, setShowFilterModal } =
-		useContext(HomesContext);
+  const { homeList, showFilterModal, setShowFilterModal } =
+    useContext(HomesContext);
 
-	const { filterList } = useContext(FilterContext);
+  const { filterList } = useContext(FilterContext);
 
-	function onCLickFilter() {
-		setShowFilterModal(true);
-	}
+  function onCLickFilter() {
+    setShowFilterModal(true);
+  }
 
-	return (
-		<>
-			<Header />
+  return (
+    <>
+      <Header />
 
-			<Container>
-				{showFilterModal && <FilterModal />}
+      <Container>
+        {showFilterModal && <FilterModal />}
 
-				<ContentContainer>
-					<FilterButton onClick={onCLickFilter}>
-						<IoFilterOutline size={22} />
-						<p>Filtros</p>
-					</FilterButton>
-					<List>
-						{filterList.length === 0 ? (
-							<div className="no_filter_message">
-								<HiOutlineEmojiSad size={40} />
-								<h1>Não há casas com o filtro aplicado.</h1>
-							</div>
-						) : filterList ? (
-							filterList.map((product, index) => (
-								<CardHouse key={index} product={product} />
-							))
-						) : (
-							homeList.map((product, index) => (
-								<CardHouse key={index} product={product} />
-							))
-						)}
-					</List>
-				</ContentContainer>
-			</Container>
-		</>
-	);
+        <ContentContainer>
+          <FilterButton onClick={onCLickFilter}>
+            <IoFilterOutline size={22} />
+            <p>Filtros</p>
+          </FilterButton>
+          <List>
+            {filterList.length === 0 ? (
+              <div className="no_filter_message">
+                <HiOutlineEmojiSad size={40} />
+                <h1>Não há casas com o filtro aplicado.</h1>
+              </div>
+            ) : filterList ? (
+              filterList.map((product, index) => (
+                <CardHouse key={index} product={product} />
+              ))
+            ) : (
+              homeList.map((product, index) => (
+                <CardHouse key={index} product={product} />
+              ))
+            )}
+          </List>
+        </ContentContainer>
+      </Container>
+    </>
+  );
 }
 
 export default Home;
